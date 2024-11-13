54c34613d249830a7ca7da79d909d14b64ca6495
iambasantarai <iambasantarai@gmail.com>
2024-11-13 21:10:48 +0545

    Read command line arguments

--- main.go
+++ main.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/iambasantarai/ga/lexer"
 	"github.com/iambasantarai/ga/token"
 )
 
 func main() {
-	input := `पूर्णांक मुख्य() {
-	       प्रदर्शन("सोच्छौ के मेरो बारे?")।
-	       फिर्ता 0।
-	   }`
+	cliArgs := os.Args
+	command := cliArgs[1]
+	sourceFilePath := cliArgs[2]
 
-	lexer := lexer.New(input)
+	if command == "ga" {
+		sourceCode, err := os.ReadFile(sourceFilePath)
+		if err != nil {
+			panic(err)
+		}
 
-	for tok := lexer.NextToken(); tok.Type != token.EOF; tok = lexer.NextToken() {
-		fmt.Printf("TokenType: %s, Literal=%q\n", tok.Type, tok.Literal)
+		lexer := lexer.New(string(sourceCode))
+
+		for tok := lexer.NextToken(); tok.Type != token.EOF; tok = lexer.NextToken() {
+			fmt.Printf("TokenType: %s, Literal=%q\n", tok.Type, tok.Literal)
+		}
+	} else {
+		fmt.Println("Unrecognized command.")
 	}
 }
