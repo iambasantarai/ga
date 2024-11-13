8ce8a36f376ba75b94339b2fccc9896eee64f55d
iambasantarai <iambasantarai@gmail.com>
2024-11-13 21:37:21 +0545

    Validate command line argument length and file extension

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/iambasantarai/ga/lexer"
 	"github.com/iambasantarai/ga/token"
@@ -10,7 +11,15 @@ import (
 
 func main() {
 	cliArgs := os.Args
+	if len(cliArgs) < 2 {
+		panic("Not enough arguments.")
+	}
+
 	sourceFilePath := cliArgs[1]
+	fileExtension := filepath.Ext(sourceFilePath)
+	if fileExtension != ".ga" {
+		panic("Unrecognized file type. File must have '.ga' extension.")
+	}
 
 	sourceCode, err := os.ReadFile(sourceFilePath)
 	if err != nil {
