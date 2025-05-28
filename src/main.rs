9341262160ec4bd1c173d9330b2390e4df3e5fcc
iambasantarai <developer.basanta@gmail.com>
2025-05-28 17:28:24 +0545

    Check source file extension before proceeding

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::{env, fs, process};
+use std::{env, fs, path, process};
 mod token;
 mod lexer;
 mod utils;
 
 use token::TokenKind;
 use lexer::Lexer;
+use path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,6 +16,13 @@ fn main() {
     }
 
     let file_path = &args[1];
+
+    let file_ext = Path::new(file_path).extension().and_then(|ext| ext.to_str());
+    if file_ext != Some("ga") {
+        eprintln!("Invalid file extension.\nPlease provide a file with the '.ga' extension.");
+        process::exit(1);
+    }
+
     let source = fs::read_to_string(file_path).expect("Can't read this file.");
 
     let mut lexer = Lexer::new(source);
